624f15b3c4a85c4ef4594e79adbd02e9862aac33
manhcuong2603 <hoangmcuong2603@gmail.com>
2023-03-18 01:34:57 +0700

    two-commit

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var menu = document.querySelector('.container-menu-two ');
+///slide 1
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -11,7 +13,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
     let i;
-    let slides = document.getElementsByClassName("slides-images");
+    let slides = document.getElementsByClassName("images-slide-background");
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
@@ -23,3 +25,16 @@ function showSlides(n) {
 setInterval(function () {
     plusSlides(1);
 }, 3000);
+
+
+////thanh menu doi mau
+window.addEventListener('scroll', function () {
+    if (this.window.pageYOffset > 50) {
+        menu.classList.add('doimenu');
+
+    }
+    else {
+        menu.classList.remove('doimenu');
+    }
+})
+
